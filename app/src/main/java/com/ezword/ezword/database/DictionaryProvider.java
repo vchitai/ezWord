6110d0f9c6eb11762fcaa37f264b37dffcb5524b
Phuc <nvinhphuc210@gmail.com>
2018-06-05 16:39:46 +0700

    Modify database

--- app/src/main/java/com/ezword/ezword/database/DictionaryProvider.java
+++ app/src/main/java/com/ezword/ezword/database/DictionaryProvider.java
@@ -1,9 +1,14 @@
 package com.ezword.ezword.database;
 
 import com.ezword.ezword.database.DictionaryContract.*;
+import com.ezword.ezword.dictionary.Dictionary;
+import com.ezword.ezword.dictionary.Word;
+
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,17 +16,27 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import java.util.ArrayList;
+
 /**
  * Created by chita on 02/06/2018.
  */
 
 public class DictionaryProvider extends ContentProvider {
+    private static DictionaryProvider mInstance = null;
     public static final  String     LOG_TAG     = DictionaryProvider.class.getSimpleName();
     private static final int        WORD       = 100;
     private static final int        WORD_ID     = 101;
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
     private DictionaryDBHelper mDictDBHelper;
     private SQLiteDatabase mDictDatabase;
+
+    public static DictionaryProvider getInstance() {
+        if (mInstance == null) {
+            mInstance = new DictionaryProvider();
+        }
+        return mInstance;
+    }
 
     static {
         sUriMatcher.addURI(DictionaryContract.CONTENT_AUTHORITY, DictionaryContract.PATH_DICTIONARY, WORD);
@@ -38,8 +53,6 @@ public class DictionaryProvider extends ContentProvider {
     @Override
     public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        mDictDBHelper.open();
-        mDictDatabase = mDictDBHelper.getMyDatabase();
         Cursor cursor;
         int match = sUriMatcher.match(uri);
 
@@ -57,7 +70,6 @@ public class DictionaryProvider extends ContentProvider {
             default:
                 throw new IllegalArgumentException("Cannot query unknown URI" + uri);
         }
-        //mDictDBHelper.close();
         return cursor;
     }
 
@@ -90,4 +102,56 @@ public class DictionaryProvider extends ContentProvider {
     public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
         return 0;
     }
+
+    public Word search(Context context, String searchPhrase)
+    {
+        mDictDBHelper = new DictionaryDBHelper(getContext());
+        mDictDBHelper.open();
+        mDictDatabase = mDictDBHelper.getMyDatabase();
+        Word res = null;
+        String []projection = new String[]{DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG,
+                DictionaryContract.DictionaryEntry.COLUMN_WORD_DEFINITION,
+                DictionaryContract.DictionaryEntry.COLUMN_WORD_TYPE,
+                DictionaryContract.DictionaryEntry.COLUMN_WORD_PHONETIC_SPELLING};
+        String []selectionArgument = new String[]{ searchPhrase + "%" };
+        String whereClause = DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG + " LIKE ?";
+        //ContentResolver contentResolver = context.getContentResolver();
+        Cursor c = this.query(DictionaryContract.DictionaryEntry.CONTENT_URI,
+                projection, whereClause, selectionArgument, DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG + " ASC" + " LIMIT 1");
+        if (c != null) {
+            c.moveToFirst();
+            res = new Word(c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG)),
+                    c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_TYPE)),
+                    c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_DEFINITION)),
+                    c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_PHONETIC_SPELLING)));
+            c.close();
+        }
+        mDictDBHelper.close();
+        return res;
+    };
+
+    public ArrayList<String> getRecommendations(Context context, String searchPhrase, Integer limit) {
+        mDictDBHelper = new DictionaryDBHelper(getContext());
+        mDictDBHelper.open();
+        mDictDatabase = mDictDBHelper.getMyDatabase();
+        ArrayList<String> res = new ArrayList<>();
+        String []projection = {DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG};
+        String []selectionArgument = { searchPhrase + "%" };
+        String whereClause = DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG + " LIKE ?";
+        //ContentResolver contentResolver = context.getContentResolver();
+        Cursor c = this.query(DictionaryContract.DictionaryEntry.CONTENT_URI,
+                projection, whereClause, selectionArgument,
+                DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG + " ASC " +"LIMIT " + limit);
+        if (c != null) {
+            c.moveToFirst();
+            while (!c.isAfterLast()) {
+                res.add(c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG)));
+                c.moveToNext();
+            }
+            c.close();
+        }
+        mDictDBHelper.close();
+        return res;
+    }
+
 }
--- app/src/main/java/com/ezword/ezword/dictionary/Dictionary.java
+++ app/src/main/java/com/ezword/ezword/dictionary/Dictionary.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 
 import com.ezword.ezword.database.DictionaryContract;
+import com.ezword.ezword.database.DictionaryProvider;
 
 import java.util.ArrayList;
 
@@ -27,44 +28,12 @@ public class Dictionary {
 
     public Word search(Context context, String searchPhrase)
     {
-        Word res = null;
-        String []projection = new String[]{DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG,
-                                DictionaryContract.DictionaryEntry.COLUMN_WORD_DEFINITION,
-                                DictionaryContract.DictionaryEntry.COLUMN_WORD_TYPE,
-                                DictionaryContract.DictionaryEntry.COLUMN_WORD_PHONETIC_SPELLING};
-        String []selectionArgument = new String[]{ searchPhrase + "%" };
-        String whereClause = DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG + " LIKE ?";
-        ContentResolver contentResolver = context.getContentResolver();
-        Cursor c = contentResolver.query(DictionaryContract.DictionaryEntry.CONTENT_URI,
-                projection, whereClause, selectionArgument, DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG + " ASC" + " LIMIT 1");
-        if (c != null) {
-            c.moveToFirst();
-            res = new Word(c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG)),
-                    c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_TYPE)),
-                    c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_DEFINITION)),
-                    c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_PHONETIC_SPELLING)));
-            c.close();
-        }
-        return res;
+        DictionaryProvider dictionaryProvider = DictionaryProvider.getInstance();
+        return dictionaryProvider.search(context, searchPhrase);
     };
 
     public ArrayList<String> getRecommendations(Context context, String searchPhrase, Integer limit) {
-        ArrayList<String> res = new ArrayList<>();
-        String []projection = {DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG};
-        String []selectionArgument = { searchPhrase + "%" };
-        String whereClause = DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG + " LIKE ?";
-        ContentResolver contentResolver = context.getContentResolver();
-        Cursor c = contentResolver.query(DictionaryContract.DictionaryEntry.CONTENT_URI,
-                projection, whereClause, selectionArgument,
-                DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG + " ASC " +"LIMIT " + limit);
-        if (c != null) {
-            c.moveToFirst();
-            while (!c.isAfterLast()) {
-                res.add(c.getString(c.getColumnIndex(DictionaryContract.DictionaryEntry.COLUMN_WORD_WORD_ENG)));
-                c.moveToNext();
-            }
-            c.close();
-        }
-        return res;
+        DictionaryProvider dictionaryProvider = DictionaryProvider.getInstance();
+        return dictionaryProvider.getRecommendations(context, searchPhrase, limit);
     }
 }
