8143fe86817347c6a0c5a0dd2f9bce0a6a5e1daf
Phuc <nvinhphuc210@gmail.com>
2018-06-30 18:55:08 +0700

    Add clock and get answer quality for Quiz

--- app/src/main/java/com/ezword/ezword/ui/main_activities/QuizActivity.java
+++ app/src/main/java/com/ezword/ezword/ui/main_activities/QuizActivity.java
@@ -2,12 +2,14 @@ package com.ezword.ezword.ui.main_activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ezword.ezword.R;
@@ -31,6 +33,10 @@ public class QuizActivity extends AppCompatActivity {
     private ArrayList<FlashCard> mFlashCards;
     private EditText textAnswer;
     private int i;
+    private TextView mTextCountDown;
+    private CountDownTimer mCountDownTimer;
+    private long mCountDownTime;
+    private long timeUsed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +49,27 @@ public class QuizActivity extends AppCompatActivity {
 
         mNextQuestion = (Button)findViewById(R.id.quiz_next_question);
         setOnClickButtonListener();
+
+        mTextCountDown = (TextView)findViewById(R.id.text_countdown_timer);
+        mCountDownTime = 15;
+        mTextCountDown.setText(String.valueOf(mCountDownTime));
+    }
+
+    private void startCountDown() {
+        mTextCountDown.setText(String.valueOf(mCountDownTime));
+        mCountDownTimer = new CountDownTimer(mCountDownTime * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTextCountDown.setText(String.valueOf(millisUntilFinished / 1000));
+                timeUsed = mCountDownTime - millisUntilFinished / 1000;
+            }
+
+            @Override
+            public void onFinish() {
+                mTextCountDown.setText("");
+                mNextQuestion.performClick();
+            }
+        }.start();
     }
 
     private void setUpFragment() {
@@ -66,38 +93,53 @@ public class QuizActivity extends AppCompatActivity {
         mNextQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textAnswer = (EditText) findViewById(R.id.word_matching_answer);
-                if (i >= mFlashCards.size()) {
-                    return;
-                }
-                if (i >= 0) {
-                    checkAnswer(String.valueOf(textAnswer.getText()), mFlashCards.get(i).getAnswer());
-                }
                 i++;
-                if (i >= mFlashCards.size()) {
-                    Intent returnIntent = new Intent();
-                    setResult(Activity.RESULT_CANCELED, returnIntent);
-                    finish();
-                    return;
+                if (mCountDownTimer != null) {
+                    mCountDownTimer.cancel();
+                    mCountDownTimer = null;
                 }
-                mQuizQuestionFragment.updateQuizView(mFlashCards.get(i));
-                textAnswer.setText("");
+                textAnswer = (EditText)findViewById(R.id.word_matching_answer);
+                if (i == 0) {
+                    mQuizQuestionFragment.updateQuizView(mFlashCards.get(i));
+                    startCountDown();
+                }
+                if (i > 0) {
+                    checkAnswer(String.valueOf(textAnswer.getText()), i - 1, timeUsed);
+                    textAnswer.setText("");
+                    if (i < mFlashCards.size()) {
+                        mQuizQuestionFragment.updateQuizView(mFlashCards.get(i));
+                        startCountDown();
+                    }
+                    else if (i == mFlashCards.size()) {
+                        Intent returnIntent = new Intent();
+                        setResult(Activity.RESULT_CANCELED, returnIntent);
+                        finish();
+                    }
+                }
             }
         });
     }
 
-    private void checkAnswer(String textAnswer, String answer) {
+    private void checkAnswer(String textAnswer, int num, long timeUsed) {
         Random rand = new Random();
         int answerQuality;
-        if (textAnswer.equalsIgnoreCase(answer)) {
-            answerQuality = rand.nextInt(5 - 3) + 2;
+        if (textAnswer.equalsIgnoreCase(mFlashCards.get(num).getAnswer())) {
+            if (timeUsed < mCountDownTime / 3)
+                answerQuality = FlashCard.ANSWER_QUALITY_PERFECT;
+            else if (timeUsed < mCountDownTime / 2)
+                answerQuality = FlashCard.ANSWER_QUALITY_CORRECT_SLOW;
+            else
+                answerQuality = FlashCard.ANSWER_QUALITY_CORRECT_VERY_SLOW;
             Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
         }
         else {
+            if (timeUsed < mCountDownTime / 2)
+                answerQuality = FlashCard.ANSWER_QUALITY_INCORRECT;
+            else
+                answerQuality = FlashCard.ANSWER_QUALITY_BLACKOUT;
             answerQuality = rand.nextInt(2);
             Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
         }
-        mFlashCards.get(i).updateAfterAnswer(answerQuality, this);
-
+        mFlashCards.get(num).updateAfterAnswer(answerQuality, this);
     }
 }
